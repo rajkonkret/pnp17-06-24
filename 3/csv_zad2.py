f44fb0406cd3cb4e7359875abed3ba26654f2d92
rajkonkret <rajkonkret660@gmail.com>
2024-06-19 13:01:04 +0200

    [3.12] update csv_zad2.py

--- 3/csv_zad2.py
+++ 3/csv_zad2.py
@@ -3,17 +3,18 @@ import csv
 fields = []  # nazwy kolumn
 rows = []  # dane z poszczególnych wierszy
 
-filename = 'records_2.csv'
+# filename = 'records_2.csv'
+filename = 'records_3.csv'
 
 with open(filename, 'r') as f:
     csvreader = csv.reader(f)
     print(csvreader)  # <_csv.reader object at 0x00000236369D58A0> iterator
-    # iterator - zwraca kolejne elemnty
+    # iterator - zwraca kolejne elementy
     # za kazym razem gdy go odczytuje zwraca następny elemnent
     # zapamiętuje gdzie skońćzył
     fields = next(csvreader)  # next() odczyt pojedynczego elemntu z iteratora
     # lista fields otrzyma dane z pierwszego wierszaw csv czyli kolumny
-    for row in csvreader:  # dalsze odczytywanie iteratora zacznie się od elemntu drugiego
+    for row in csvreader:  # dalsze odczytywanie iteratora zacznie się od elementu drugiego
         # print(row)
         rows.append(row)
 
