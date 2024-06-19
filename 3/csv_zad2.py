aad1e5043afe33d001fc830d76eaac1858d6fe32
rajkonkret <rajkonkret660@gmail.com>
2024-06-19 12:54:34 +0200

    [3.11] update csv_zad2.py

---
+++ 3/csv_zad2.py
@@ -0,0 +1,23 @@
+import csv
+
+fields = []  # nazwy kolumn
+rows = []  # dane z poszczególnych wierszy
+
+filename = 'records_2.csv'
+
+with open(filename, 'r') as f:
+    csvreader = csv.reader(f)
+    print(csvreader)  # <_csv.reader object at 0x00000236369D58A0> iterator
+    # iterator - zwraca kolejne elemnty
+    # za kazym razem gdy go odczytuje zwraca następny elemnent
+    # zapamiętuje gdzie skońćzył
+    fields = next(csvreader)  # next() odczyt pojedynczego elemntu z iteratora
+    # lista fields otrzyma dane z pierwszego wierszaw csv czyli kolumny
+    for row in csvreader:  # dalsze odczytywanie iteratora zacznie się od elemntu drugiego
+        # print(row)
+        rows.append(row)
+
+print("Fields", fields)
+print("Rows", rows)
+# Fields ['name', 'branch', 'year', 'cgpa']
+# Rows [['Radek', 'Coe', '2', '9.1']]
