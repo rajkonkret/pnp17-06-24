ca1cb834380f2bc43a032db1a52da483bca015bb
rajkonkret <rajkonkret660@gmail.com>
2024-06-19 14:45:06 +0200

    [3.18] add fun3.py

---
+++ 3/fun3.py
@@ -0,0 +1,29 @@
+a = 10
+b = 10
+
+
+def dodaj():
+    a = 7  # zmienne lokalne
+    b = 9
+    print(a + b)
+
+
+def dodaj2():
+    print(a + b)
+
+
+def dodaj3():
+    global a
+    a = 9  # a jest globalne, zmieni wartość globalnej zmiennej a
+    b = 8
+    print(a + b)
+
+
+print(f"Wartość a z góry wynosi {a}")  # Wartość a z góry wynosi 10
+dodaj()  # 16
+print(f"Wartość a z góry wynosi {a}")  # Wartość a z góry wynosi 10
+dodaj2()  # 20
+print(f"Wartość a z góry wynosi {a}")  # Wartość a z góry wynosi 10
+dodaj3()  # 17
+print(f"Wartość a z góry wynosi {a}")  # Wartość a z góry wynosi 9
+dodaj2()  # 19
