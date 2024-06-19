f3a7dc3fbab6ff194efb4ba442d245bca84dd742
rajkonkret <rajkonkret660@gmail.com>
2024-06-19 09:51:27 +0200

    [3.4] add kalkulator.py

---
+++ 3/kalkulator.py
@@ -0,0 +1,6 @@
+# napisac prosty kalkulator z wykorzystaniem pętli while True
+# menu z dostępnymi opcjami
+# pobrać opcje z menu
+# pobrać liczby
+# wyświetlić wynik działania
+# obsłużyć wyjątki (błedy)
